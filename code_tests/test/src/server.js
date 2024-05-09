f5f06dcbcab06b2ece9882d8ad06c09aa2e2f5a5
Abi Prescott <abiprescott586@icloud.com>
2024-05-09 13:02:01 -0300

    created password module

--- code_tests/test/src/server.js
+++ code_tests/test/src/server.js
@@ -1,18 +1,30 @@
 
-const express = require('express');
-const path = require('path');
+const express = require('express')
+const path = require('path')
 const ethers = require('ethers')
+const crypto = require('node:crypto');
+const { hexConcat } = require('ethers/lib/utils');
+//const routes = require('./routes/api')
+//const bodyParser = require('body-parser')
 
 const app = express();
 
+//app.use(bodyParser.urlencoded({extended: false}))
+//app.use(express.json())
+//app.use('/', routes)
+
 app.use(express.static('../public'))
 app.use(express.json())
+
+
+
+
 
 app.listen(8000, () => {
     console.log('Server is running at port 8000');
 })
 
-async function generateKeys (req, res, next) {
+async function generateKeys(req, res, next) {
     const wallet = ethers.Wallet.createRandom();
     const privateKey = wallet.privateKey
     const publicKey = wallet.publicKey
@@ -21,15 +33,15 @@ async function generateKeys (req, res, next) {
 
 
     res.json({
-        privateKey:{privateKey},
-        publicKey:{publicKey},
-        walletAddress:{walletAddress},
-        phrase:{phrase}
+        privateKey: { privateKey },
+        publicKey: { publicKey },
+        walletAddress: { walletAddress },
+        phrase: { phrase }
     });
-    next(); 
+    next();
 }
 
-async function importWallet (req, res, next) {
+async function importWallet(req, res, next) {
     const { phrase } = req.body
 
     const wallet = ethers.Wallet.fromMnemonic(phrase);
@@ -42,16 +54,33 @@ async function importWallet (req, res, next) {
     console.log(wallet.address)
 
     res.json({
-        privateKey:{privateKey},
-        publicKey:{publicKey},
-        walletAddress:{walletAddress},
+        privateKey: { privateKey },
+        publicKey: { publicKey },
+        walletAddress: { walletAddress },
     })
     next()
 
 }
 
+function hashPassword(req, res, next) {
+    var password = req.url
+    var salt = crypto.randomBytes(16) //recomended by geeksforgeeks
+    const iterations = 600000
+    const keylen = 128 //choice is arbritrary
+    const digest = 'sha512' //choice is arbritrary
+
+    var hashed_password = crypto.pbkdf2Sync(password, salt, iterations, keylen, digest)
+    res.send(hashed_password.toString('hex'))
+    
+    next()
+}
+
+
+
+app.post('/create_password', hashPassword)
+
 app.get('/', (req, res) => {
-   res.send('<p>hello</p>') 
+    res.send('<p>hello</p>')
 })
 
 app.get('/wallet', generateKeys)
